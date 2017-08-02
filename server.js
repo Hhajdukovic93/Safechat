b4e889beb841c432dec51bd9a424872e76905f0d
Hhajdukovic93 <hhajdukovic93@gmail.com>
2017-08-02 19:27:37 +0200

    Add message sharing functionality

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+//  Array for users
+users = [];
+//  Array for connection
+connections = [];
+
+//  Server listening on port 3000
+server.listen(process.env.PORT || 3000);
+console.log('Server running...');
+
+
+//  Server sends to clients
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+
+//  Socket communication
+io.sockets.on('connection', function(socket){
+
+	// After every new connection, increase/push connection array with new one
+	connections.push(socket);
+	// Display number of new sockets/connections to server in console
+	console.log('Connected: %s sockets connected', connections.length)
+
+	// Disconnect 
+	socket.on('disconnect', function(data){
+		// After disconnection, decrease/pop connection array by one
+		connections.splice(connections.indexOf(socket), 1);
+		// Again, display number of current sockets/connections
+		console.log('Disconnected: %s sockets connected', connections.length);
+	});
+	
+
+	//  Send messages
+	socket.on('send message', function(data){ //  message is cought here
+		console.log(data); // message written in console
+		io.sockets.emit('new message', {msg: data});  // after that it is emmited
+	});
+		
+});
