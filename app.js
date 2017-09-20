f049ba282ad08de8eac2448459913db85b0ed07a
Hhajdukovic93 <hhajdukovic93@gmail.com>
2017-09-20 18:54:58 +0200

    Directories restructure and unused files deleted

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var path = require('path');
 var http = require('http');
 var fs   = require('fs');
 
-
 //  ExpressJS module
 var express = require('express');
 //  Make instance of express application
@@ -26,68 +25,6 @@ var bodyParser = require('body-parser');
 
 //  MONGO DB User register
 //var Person = require('./Person.js');
-
-/*
-//  Piler
-var piler = require("piler");
-
-var clientjs = piler.createJSManager();
-var clientcss = piler.createCSSManager();
-
-
-clientjs.bind(app,server); // Make sure to bind to both Express and the server!
-clientcss.bind(app,server);
-
-//clientcss.addFile(__dirname + "/style.css");
-
-clientjs.addUrl("http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js");
-//clientjs.addFile(__dirname + "/client/hello.js");
-
-if (process.env.NODE_ENV === 'development') {
-    clientjs.liveUpdate(clientcss, require('socket.io')(server));
-}
-
-clientjs.addOb({ VERSION: "1.0.0" });
-
-clientjs.addExec(function() {
-    alert("Hello browser" + window.navigator.appVersion);
-});
-
-*/
-
-/*  ------------------------------------------------ *\
-*   MY MODULS
-\*  ------------------------------------------------ */
-
-//  Node module for cryptography
-var cryptographyMethods = require('./public/javascripts/serverCryptography');
-
-//  Node module for steganography
-var steganographyMethods = require('./public/javascripts/serverSteganography.js');
-
-
-
-
-// Export node modules to CLIENT
-//var noderequire = require('node-require');
- 
-// replace <package name>, ... with all package names you would like to export to the client
-// <options> is an object, currently the only supported property is min:true
-// if options.min===true, then a minified version of the package will be loaded if available
-
-//noderequire.export(app,__dirname,["<package name>",...][,<options>);
-
-//noderequire.export(app,__dirname,["<test>"]);
-
-
-
-//var mymodule = require('./mymodule.js');
-
-//clientjs.addFile(__dirname + "/mymodule.js");
-
-//mymodule.test();  
-
-//sys.puts(mymodule.test());
 
 
 
@@ -178,41 +115,11 @@ app.get('/', function(req, res) {
 	//res.redirect('/views/pero.html');
 });
 
-
-//  Send JS script to client
-app.get('/js/bundle.js', function(req,res) {
-	res.sendFile(__dirname + '/js/bundle.js');
-});
-
-//  Send JS script to client
-app.get('/matrix.png', function(req,res) {
-	res.sendFile(__dirname + '/matrix.png');
-});
-
-
-
-
-//  Send JS script to client
-app.get('/stegoobject.png', function(req,res) {
-	//res.type('html').status(200);
-	//res.send('Pero je pozvan');
-
-
-	// Tu odradim promjene
-
-	//steganographyMethods.writeInImage();
-	res.sendFile(__dirname + '/stegoobject.png');
-
-	//  Vratim stranicu koja sadrzava sliku
-});
 /*
 //  Send JS script to client
 app.get('/public/javascripts/steganography.js', function(req,res) {
 	res.sendFile(__dirname + '/public/javascripts/steganography.js');
 });
-
-*/
-/*
 app.use('/login', function(req, res) {
 	res.type('html').status(200);
 	res.sendFile(__dirname + '/login.html');
@@ -221,11 +128,7 @@ app.use('/chat', function(req, res) {
 	res.type('html').status(200);
 	res.sendFile(__dirname + '/chat.html');
 });
-*/
 
-
-
-/*
 //  ROUTE FOR CREATING USERS
 app.use('/create', function(req, res) {
 	var newPerson = new Person ({
@@ -249,10 +152,6 @@ app.use('/created', function(req, res) {
 	res.send('User created');
 });
 
-*/
-
-
-/*
 //  All kind of handle form !!!
 app.use('/handleForm', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
@@ -267,18 +166,6 @@ app.use('/handleForm', function(req, res) {
 
 
 
-
-/*  ------------------------------------------------ *\
-*	STEGANOGRAPHY
-\*  ------------------------------------------------ */
-
-//  Invoke steagnography method from my module
-//steganographyMethods.writeInImage();
-
-
-
-
-
 /*  ------------------------------------------------ *\
 *   SOCKET COMMUNICATION
 \*  ------------------------------------------------ */
@@ -287,16 +174,6 @@ app.use('/handleForm', function(req, res) {
 users = [];
 //  Array for connections
 connections = [];
-
-
-//  Images
-var plainImage,
-	stegoImage;
-
-//  String for  cipher and plain text
-var plain,
-	cipher;
-
 
 io.on('connection', function(socket) {
 
@@ -307,7 +184,6 @@ io.on('connection', function(socket) {
 
 	//  Users
 	socket.on('new user', function(data, callback) { 
-
 		callback(true);
 		//  Get username from method, preciselly from method data
 		socket.username = data;
@@ -322,91 +198,26 @@ io.on('connection', function(socket) {
 		io.emit('get users', users)
 	}
 
-
 	//  Messages
 	socket.on('send message', function(data)  { 
-		
-		//  STEGO is coming
-		//  Message written in console from client to server - crypted inside STEGOOBJECT
-		console.log("--- STEGOOBJECT is here ---"); 
-
+		//  Stegoobject from client
+		console.log("Stegoobject came to server from client"); 
 		stegoImage = data;
-/*
-
-
-
-		// Revert steganography
-		//cipher = steg.makePlainFromCipher(data); // #IMG defautl, BUT cover
-
-
-		//cipher = steganographyMethods.makePlainFromCipherSERVER(data);
-		
-		//  Decrypt message on server side
-		console.log("Decrypting in process..."); 
-		// Takae care about this problem !!!!!!!!!!
-		plain = cryptographyMethods.DecryptMethod(cipher);
-		//  Display decrypted message in console
-		console.log("Decrypted message : " + plain); 
-
-
-		//  Add image with secret message to response
-		stegoobject = ".outServer.png";
-		console.log('Server - image : ' + stegoobject);
-
-		//   Crypt again
-		data = cryptographyMethods.EncryptMethod(data);
-		
-		//  Send stego object as route
-		//  I have stegoobject now
-
-		//  TO DO : Make stegoobjcet (image)  transferable
-		// Decode
-        
-*/
-
-		//  NEW MESSAGE can be reviced as image via route
-		//  After that it is emmited
+		//  Emmit stegoobject to all clients
 		io.emit('new message', { user: socket.username, image: stegoImage });
 	});
 
-
 	// Disconnecting 
 	socket.on('disconnect', function(data){
-		
 		//  Delete username from user list after disconnected
 		users.splice(users.indexOf(socket.username), 1);
 		//  Display diconnected user
 		console.log('Disconnected user: %s', socket.username);
-
 		//  Update username list after change  ( DELETE )
 		updateUsernames();
-
 		// After disconnection, decrease/pop connection array by one
 		connections.splice(connections.indexOf(socket), 1);
 		// Again, display number of current sockets/connections
 		console.log('Disconnected: %s sockets connected', connections.length);
 	});
-
 });
-
-
-
-
-
-
-
-
-/*  ------------------------------------------------ *\
-*	TESTING
-\*  ------------------------------------------------ */
-/*
-test = function() {
-	console.log("I am inside of server.js..");
-}
-
-exports.test = test;
-
-*/
-
-
-
---
+++ public/javascripts/safechat.js
@@ -0,0 +1,639 @@
+$(function() {
+
+	//  Object for "on" and "emit"
+	var socket = io.connect();
+
+	//  Areas
+	var cipherArea = $('#cipherArea'),
+	    userArea = $('#userArea'),
+	    messageArea = $('#messageArea');
+
+	//  Forms
+	var cipherForm = $('#cipherForm'),	
+	    userForm = $('#userForm'),
+	    messageForm = $('#messageForm');
+
+	//  Inputs
+	var cipher = $('#cipher'),
+	    username = $('#username'),
+	    message = $('#message');
+
+	//  Text areas
+	var chat = $('#chat'),
+	    users = $('#users');
+
+	//  Cryptography strings
+	var plainText,
+		  cryptText;
+
+
+
+	/*  ------------------------------------------------ *\
+	*   (1) CIPHER 
+	\*  ------------------------------------------------ */
+
+	//  Method for secret key
+	cipherForm.submit(function(event) {
+
+		// Display message in console after new user is online
+		console.log('Secret key submitted');
+
+		var userSecretKeyInput = cipher.val();
+		console.log(userSecretKeyInput);
+
+		if (userSecretKeyInput == 'enigma') {
+			cipherArea.hide();
+			userArea.show();
+			console.log("Succed with cipher key");
+		} 
+		else {
+			console.log("Failed with cipher key");
+		}
+
+		//  Make username input form empty again
+		cipher.val('');
+		event.preventDefault();
+	});
+
+
+
+	/*  ------------------------------------------------ *\
+	*   (2) USERS
+	\*  ------------------------------------------------ */
+
+	//  Method for user login
+	userForm.submit(function(event) {
+
+		// Display message in console after new user is online
+		console.log('User : ' + username.val() + ' logged in chat');
+		//  Send new user data to server
+		socket.emit('new user', username.val(), function(data){
+			//  Proceed from login screen to chat screen
+			if(data) {
+				userArea.hide();
+				messageArea.show();
+			}
+		});
+		//  Make username input form empty again
+		username.val('');
+		event.preventDefault();
+
+
+		//  TO DO : interact with MongoDB
+	});
+
+	//  List of active user - add to user list in chat app
+	socket.on('get users', function(data) {
+					
+		//  Array for all user/clients active on chat
+		var userList = '';
+		//  Go through user list array and make list item for every user
+		for(i = 0; i < data.length; i++) {
+			userList += '<li class="list-group-item">' + data[i] + '</li>'
+		}
+		//  Write userList unorder list to userlist in chat app
+		users.html(userList);
+	});
+
+
+
+	/*  ------------------------------------------------ *\
+	*   (3) MESSAGES 
+	\*  ------------------------------------------------ */
+
+	// Method for user communication
+	messageForm.submit(function(event) {
+			
+		//  Get message from FORM textarea
+		plainText = message.val();
+
+		//  Do crypting
+		cryptText = Encrypt(plainText);
+
+		// Make stegoobject
+        cover.src = steg.encode(cryptText, img, {"width": img.width, "height": img.height});
+        stegoObject = cover.src;
+
+		// Emit message - send message from client to server (STEGOOBJECT)
+		socket.emit('send message', stegoObject);
+
+		// Make textarea empty after submit, ready for new message by user
+		message.val('');
+		event.preventDefault();
+	});
+
+
+	// Get new message and display it in chat window by adding new DIV
+	socket.on('new message', function(data) {
+
+		console.log("Stigao je STEGOOBJEKT");
+
+		// Define user
+		user = data.user;
+
+		// Define stegoobject
+        cover.src = data.image;
+        cover.title = "Sa servera";
+        stegoObject = cover.src;
+
+        setTimeout(function(){
+	        //  Make revert steganography
+	        cryptText = steg.decode(stegoObject);
+	        //  Decrypt
+			    plainText = Decrypt(cryptText);
+			    chat.append('<div class="well"><strong>' + user + '</strong>: ' + plainText + '</div>');      
+        }, 100);
+	});
+});
+
+
+
+/*  ------------------------------------------------ *\
+*	STEGANOGRAPHY
+\*  ------------------------------------------------ */
+
+;(function (name, context, factory) {
+
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = factory();
+  } 
+  else if (typeof define === "function" && define.amd) {
+    define(factory);
+  } 
+  else {
+    context[name] = factory();
+  }
+})("steg", this, function () {
+
+
+  var Cover = function Cover() {};
+
+  var util = {
+
+    "isPrime" : function(n) {
+
+      if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false;
+      if (n%2===0) return (n===2);
+      if (n%3===0) return (n===3);
+
+      var m=Math.sqrt(n);
+
+      for (var i=5;i<=m;i+=6) {
+        if (n%i===0) return false;
+        if (n%(i+2)===0) return false;
+      }
+
+      return true;
+    },
+    "findNextPrime" : function(n) {
+      for(var i=n; true; i+=1)
+        if(util.isPrime(i)) return i;
+    },
+    "sum" : function(func, end, options) {
+      var sum = 0;
+      options = options || {};
+      for(var i = options.start || 0; i < end; i+=(options.inc||1))
+        sum += func(i) || 0;
+
+      return (sum === 0 && options.defValue ? options.defValue : sum);
+    },
+    "product" : function(func, end, options) {
+      var prod = 1;
+      options = options || {};
+      for(var i = options.start || 0; i < end; i+=(options.inc||1))
+        prod *= func(i) || 1;
+
+      return (prod === 1 && options.defValue ? options.defValue : prod);
+    },
+    "createArrayFromArgs" : function(args,index,threshold) {
+      var ret = new Array(threshold-1);
+      for(var i = 0; i < threshold; i+=1)
+        ret[i] = args(i >= index ? i+1:i);
+
+      return ret;
+    },
+    "loadImg": function(url) {
+      var image = new Image();
+      image.src = url;
+      while(image.hasOwnProperty('complete') && !image.complete) {}
+      return image;
+    }
+  };
+  Cover.prototype.config = {
+    "t": 3,
+    "threshold": 1,
+    "codeUnitSize": 16,
+    "args": function(i) { return i+1; },
+    "messageDelimiter": function(modMessage,threshold) {
+              var delimiter = new Array(threshold*3);
+              for(var i = 0; i < delimiter.length; i+=1)
+                delimiter[i] = 255;        
+              return delimiter;
+            },
+    "messageCompleted": function(data, i, threshold) {
+            var done = true;
+             for(var j = 0; j < 16 && done; j+=1) {
+                done = (done) && (data[i+j*4] == 255);                        
+              }
+              return done;
+            }
+  };
+  Cover.prototype.getHidingCapacity = function(image, options) {
+
+    options = options || {};
+    var config = this.config;
+
+    var width = options.width || image.width,
+        height = options.height || image.height,
+        t = options.t || config.t,
+        codeUnitSize = options.codeUnitSize || config.codeUnitSize;
+        return t*width*height/codeUnitSize >> 0;
+  };
+
+
+
+  /*  ------------------------------------------------ *\
+  *   ENCODE
+  \*  ------------------------------------------------ */
+
+  Cover.prototype.encode = function(message, image, options) {
+
+    console.log("(1) ENCODE");
+
+    if(image.length) {
+      image = util.loadImg(image);
+      console.log("Image length : " + image.length);
+    }
+
+    options = options || {};
+    var config = this.config;
+
+    var t = options.t || config.t,
+      threshold = options.threshold || config.threshold,
+      codeUnitSize = options.codeUnitSize || config.codeUnitSize,
+      prime = util.findNextPrime(Math.pow(2,t)),
+      args = options.args || config.args,
+      messageDelimiter = options.messageDelimiter || config.messageDelimiter;
+
+    console.log("(1) T variable : " + t);
+
+    if(!t || t < 1 || t > 7) throw "Error: Parameter t = " + t + " is not valid: 0 < t < 8";
+
+    //  Make CANVAS
+    var shadowCanvas = document.createElement('canvas'),
+        shadowCtx = shadowCanvas.getContext('2d');
+
+
+    //  Canvas properties
+    shadowCanvas.style.display = 'none';
+    shadowCanvas.width = options.width || image.width;
+    console.log("(1) Canvas width : " + shadowCanvas.width);
+    shadowCanvas.height = options.height || image.height;
+    console.log("(1) Canvas height : " + shadowCanvas.height);
+
+    if(options.height && options.width) {
+      shadowCtx.drawImage(image, 0, 0, options.width, options.height );
+    } 
+    else {
+      shadowCtx.drawImage(image, 0, 0);
+    }
+
+    var imageData = shadowCtx.getImageData(0, 0, shadowCanvas.width, shadowCanvas.height),
+        data = imageData.data;
+
+    var bundlesPerChar = codeUnitSize/t >> 0,
+      overlapping = codeUnitSize%t,
+      modMessage = [],
+      decM, oldDec, oldMask, left, right,
+      dec, curOverlapping, mask;
+
+    var i, j;
+    //  Convert text to numbers
+    for(i=0; i <= message.length; i+=1) {
+
+      //  Get ASCII
+      dec = message.charCodeAt(i) || 0;
+      curOverlapping = (overlapping*i)%t;
+
+      if(curOverlapping > 0 && oldDec) {
+        mask = Math.pow(2,t-curOverlapping) - 1;
+        oldMask = Math.pow(2, codeUnitSize) * (1 - Math.pow(2, -curOverlapping));
+        left = (dec & mask) << curOverlapping;
+        right = (oldDec & oldMask) >> (codeUnitSize - curOverlapping);
+        modMessage.push(left+right);
+
+        if(i<message.length) {
+          mask = Math.pow(2,2*t-curOverlapping) * (1 - Math.pow(2, -t));
+          for(j=1; j<bundlesPerChar; j+=1) {
+            decM = dec & mask;
+            modMessage.push(decM >> (((j-1)*t)+(t-curOverlapping)));
+            mask <<= t;
+          }
+          if((overlapping*(i+1))%t === 0) {
+            mask = Math.pow(2, codeUnitSize) * (1 - Math.pow(2,-t));
+            decM = dec & mask;
+            modMessage.push(decM >> (codeUnitSize-t));
+          }
+          else if(((((overlapping*(i+1))%t) + (t-curOverlapping)) <= t)) {
+            decM = dec & mask;
+            modMessage.push(decM >> (((bundlesPerChar-1)*t)+(t-curOverlapping)));
+          }
+        }
+      }
+      else if(i<message.length) {
+        mask = Math.pow(2,t) - 1;
+        for(j=0; j<bundlesPerChar; j+=1) {
+          decM = dec & mask;
+          modMessage.push(decM >> (j*t));
+          mask <<= t;
+        }
+      }
+      oldDec = dec;
+    }
+
+    // Write Data
+    var offset, index, subOffset, delimiter = messageDelimiter(modMessage,threshold),
+      q, qS;
+
+    for(offset = 0; (offset+threshold)*4 <= data.length && (offset+threshold) <= modMessage.length; offset += threshold) {
+      qS=[];
+
+      for(i=0; i<threshold && i+offset < modMessage.length; i+=1) {
+        q = 0;
+
+        for(j=offset; j<threshold+offset && j<modMessage.length; j+=1)
+          q+=modMessage[j]*Math.pow(args(i),j-offset);
+        qS[i] = (255-prime+1)+(q%prime);
+      }
+
+      for(i=offset*4; i<(offset+qS.length)*4 && i<data.length; i+=4)
+        data[i+3] = qS[(i/4)%threshold];
+      
+      subOffset = qS.length;
+    }
+    // Write message-delimiter
+    for(index = (offset+subOffset); index-(offset+subOffset)<delimiter.length && (offset+delimiter.length)*4<data.length; index+=1)
+      data[(index*4)+3]=delimiter[index-(offset+subOffset)];
+
+    // Clear remaining data
+    for(i=((index+1)*4)+3; i<data.length; i+=4) data[i] = 255;
+
+    imageData.data = data;
+    shadowCtx.putImageData(imageData, 0, 0);
+
+    return shadowCanvas.toDataURL();
+  };
+
+
+
+  /*  ------------------------------------------------ *\
+  *   DECODE
+  \*  ------------------------------------------------ */
+
+  Cover.prototype.decode = function(image, options) {
+
+    console.log("(2) DECODE");
+
+    if(image.length) {
+      image = util.loadImg(image);
+    }
+
+    options = options || {};
+    var config = this.config;
+    
+    var t = options.t || config.t,
+      threshold = options.threshold || config.threshold,
+      codeUnitSize = options.codeUnitSize || config.codeUnitSize,
+      prime = util.findNextPrime(Math.pow(2, t)),
+      args = options.args || config.args, 
+      messageCompleted = options.messageCompleted || config.messageCompleted;
+
+    console.log("(2) T : " + t);
+
+    if(!t || t < 1 || t > 7) throw "Error: Parameter t = " + t + " is not valid: 0 < t < 8";
+      
+    var shadowCanvas = document.createElement('canvas'),
+        shadowCtx = shadowCanvas.getContext('2d');
+
+    shadowCanvas.style.display = 'none';
+    shadowCanvas.width = options.width || image.width;
+    console.log("(2) Canvas width : " + shadowCanvas.width);
+    shadowCanvas.height = options.width || image.height;
+    console.log("(2) Canvas height : " + shadowCanvas.height);
+
+
+    if(options.height && options.width) {
+      shadowCtx.drawImage(image, 0, 0, options.width, options.height );
+    } 
+    else {
+      shadowCtx.drawImage(image, 0, 0);
+    }
+
+    var imageData = shadowCtx.getImageData(0, 0, shadowCanvas.width, shadowCanvas.height),
+        data = imageData.data,
+        modMessage = [],
+        q;
+
+    var i, k, done;
+
+    if (threshold == 1) {
+
+      // Check for message end
+      for(i=3, done=false; !done && i<data.length && !done; i+=4) { // true = !done(false)
+        done = messageCompleted(data, i, threshold);
+
+        if(!done) { 
+          modMessage.push(data[i]-(255-prime+1));
+        }
+      }
+    } 
+
+    // Look for hidden message in stegoobject
+    var message = "", 
+        charCode = 0, 
+        bitCount = 0,   
+        mask = Math.pow(2, codeUnitSize)-1;
+
+    for(i = 0; i < modMessage.length; i+=1) {
+
+      charCode += modMessage[i] << bitCount;
+
+      bitCount += t;
+
+      if(bitCount >= codeUnitSize) {
+
+        message += String.fromCharCode(charCode & mask);
+        bitCount %= codeUnitSize;
+        charCode = modMessage[i] >> (t-bitCount);
+      }
+
+    }
+
+    if(charCode !== 0) message += String.fromCharCode(charCode & mask);
+    return message;
+  };
+  return new Cover();
+});
+
+
+
+
+
+/*  ------------------------------------------------ *\
+*	ENCRYPTING METHOD 
+\*  ------------------------------------------------ */
+
+function Encrypt(plainText) {
+
+	console.log("Plain text : " + plainText);
+
+	//  Array with induvidual chars, split strings in chars
+	var array    = [],
+	    newArray = [];
+
+
+	//  Uppercase whole text because of matematical operation on chars
+	var uppercasePlainText = plainText.toUpperCase();
+	//  Cast string in chars array
+	array = uppercasePlainText.split("");
+	//  Array length is zero in start (logic)
+	var arraylength = 0;
+
+	for (var i = 0; i < array.length; i++) {
+		
+		//  Output char by char in array
+		//console.log("Slovo : " + array[i] + "   ASCII :" + array[i].charCodeAt());
+
+		//  If there is two empty spaces in row that means that text is over
+		if((array[i-1]==' ')&&(array[i]==' ')) {
+			
+			//  Output important message
+			console.log("Enter end!");
+			//  Decrease array length because in this mode after last character first empty 
+			//  space means end of the text and that is one char more than it is needed
+			arraylength--;
+			// First empty space at the end of message, delete it
+			newArray.pop();
+
+			//  (If there is case of two empty spaces in a row) break the loop, because 
+			//  text is over
+			break;
+		}
+
+		else {
+			//  Increase array length in normal situation
+			arraylength++;
+			//  Put plan text in array for scrypt text, just in case, because than is not empty
+			newArray.push(array[i]);
+		}
+	}
+
+
+	for (var j = 0; j < arraylength; j++) {
+
+		var oldCharacter = String.fromCharCode(array[j].charCodeAt()); 
+
+		if (oldCharacter.charCodeAt() > 64) {
+
+			//  Calculate new character - KEY SHIFT
+			var newCharacter = String.fromCharCode(oldCharacter.charCodeAt() + 13);
+
+			//  Take care about situation whene NEW character is greater than Z, it must go from A again
+			if(newCharacter.charCodeAt() > 90)
+			{
+				newCharacter = String.fromCharCode(newCharacter.charCodeAt() - 26);
+			}
+			newArray[j] = newCharacter;
+		}
+						
+		else {
+			newArray[j] = oldCharacter;
+		}
+	}
+
+	cryptText = newArray.toString();
+	cryptText = cryptText.replace(/,/g, "");
+
+	console.log("Crypt text : " + cryptText);
+
+	return cryptText;
+}
+
+
+
+/*  ------------------------------------------------ *\
+*	DECRYPTING METHOD 
+\*  ------------------------------------------------ */
+
+function Decrypt(cryptText) {
+
+	console.log("Crypt text : " + cryptText);
+
+	//  Array with induvidual chars, split strings in chars
+	var array    = [],
+		newArray = [];
+
+	//  Uppercase whole text because of matematical operation on chars
+	var uppercaseCryptText = cryptText.toUpperCase();
+	//  Cast string in chars array
+	array = uppercaseCryptText.split("");
+	//  Array length is zero in start (logic)
+	var arraylength = 0;
+
+	//  Go through array and check for ending
+	for (var i = 0; i < array.length; i++) {
+		//  Output char by char in array
+		//console.log("Slovo : " + array[i] + "   ASCII :" + array[i].charCodeAt());
+
+		//console.log(array[i]);
+
+		if( (array[i-1]==' ') && (array[i]==' ') ) {
+			console.log("Enter end!");
+			arraylength--;
+			newArray.pop();
+			break;
+		}
+
+		else{
+			arraylength++;
+			newArray.push(array[i]);
+		}
+	}
+
+
+	//  Fill new array with crypted content
+	for (var j = 0; j < (arraylength); j++) {
+
+		var oldCharacter = String.fromCharCode(array[j].charCodeAt());
+		//console.log("OLD : " + oldCharacter.charCodeAt());
+
+		if (oldCharacter.charCodeAt() > 64) {
+
+			//  Calculate new character  KEY SHIFT
+			var newCharacter = String.fromCharCode(oldCharacter.charCodeAt() - 13);
+
+			//  Take care about situation whene NEW character is greater than Z, it musto go from A again
+			if(newCharacter.charCodeAt() < 65)
+			{
+				newCharacter = String.fromCharCode(newCharacter.charCodeAt() + 26);
+			}
+			newArray[j] = newCharacter;
+		}
+
+		else {
+			newArray[j] = oldCharacter;
+		}
+
+	}
+
+	//  Make string from array for display purpose
+	var plainText = newArray.toString();
+	//  Char in array is dividede by semicolos so we need to erase semicolo
+	plainText = plainText.replace(/,/g, "");
+
+	console.log("Plain text : " + plainText);
+
+	return plainText;
+}
+
+// --------------------------------------------------------------------------------------------- //
